06ea2361ffc461bb7643f5596ab62b2ac45c7d71
GeforceLee <liyunlong880601@gmail.com>
2013-09-23 15:36:27 +0800

    init ios

---
+++ Client/Client/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Client
+//
+//  Created by xiaochuan on 13-9-23.
+//  Copyright (c) 2013年 xiaochuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Client/Client/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Client
+//
+//  Created by xiaochuan on 13-9-23.
+//  Copyright (c) 2013年 xiaochuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
