d4ad961ee19c60f30dc71300b50032430d17322a
GeforceLee <liyunlong880601@gmail.com>
2013-10-11 12:25:34 +0800

    add proto buf

--- Client/Client/lib/PomeloClient.h
+++ Client/Client/lib/PomeloClient.h
@@ -123,6 +123,9 @@ typedef enum{
     NSDictionary *_serverProtos;
     
     NSDictionary *_dict;    // route string to code
+    
+    NSInteger _protoVersion;
+    
     NSDictionary *_abbrs;   // code to route string
     
 }
